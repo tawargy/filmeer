882a94e0b096e8a10ab0cc08d2ed8724e47853d4
Mohamed El Tawargy <tawargy.dev@gmail.com>
2023-09-03 12:33:15 +0300

    fix conflict

--- server/src/datastore/sql/index.ts
+++ server/src/datastore/sql/index.ts
@@ -1,131 +1,104 @@
-import path from 'path'
-import {open, Database} from 'sqlite'
-import sqlite3 from 'sqlite3'
-import {Filmes} from './filmes'
-import {Users} from './user'
-import {WatcheLib} from './watchelib'
-import {Filme, User, Mywatched, Mylibrary} from '../../types'
+import path from "path";
+import { open, Database } from "sqlite";
+import sqlite3 from "sqlite3";
+import { Filmes } from "./filmes";
+import { Users } from "./user";
+import { WatcheLib } from "./watchelib";
+import { Filme, User, Mywatched, Mylibrary } from "../../types";
 
 export class SqlDataStore {
-  private db!: Database<sqlite3.Database, sqlite3.Statement>
-  private filmeRepo!: Filmes
-  private userRepo!: Users
-  private watchelibRepo!: WatcheLib
+  private db!: Database<sqlite3.Database, sqlite3.Statement>;
+  private filmeRepo!: Filmes;
+  private userRepo!: Users;
+  private watchelibRepo!: WatcheLib;
 
   public async openDb() {
     //open the database
     this.db = await open({
-      filename: path.join(__dirname, 'filmer.sqlite'),
+      filename: path.join(__dirname, "filmer.sqlite"),
       //filename: '/tmp/database.db',
       driver: sqlite3.Database,
-    })
-    this.db.run('PRAGMA foreign_keys=ON;')
+    });
+    this.db.run("PRAGMA foreign_keys=ON;");
     await this.db.migrate({
-      migrationsPath: path.join(__dirname, 'migrations'),
-    })
-    this.filmeRepo = new Filmes(this.db)
-    this.userRepo = new Users(this.db)
-    this.watchelibRepo = new WatcheLib(this.db)
-    return this
+      migrationsPath: path.join(__dirname, "migrations"),
+    });
+    this.filmeRepo = new Filmes(this.db);
+    this.userRepo = new Users(this.db);
+    this.watchelibRepo = new WatcheLib(this.db);
+    return this;
   }
   // User========
   createUser(user: User): Promise<void> {
-    return this.userRepo._createUser(user)
+    return this.userRepo._createUser(user);
   }
   getUserById(id: string): Promise<User | undefined> {
-    return this.userRepo._getUserById(id)
+    return this.userRepo._getUserById(id);
   }
   getUserByEmail(email: string): Promise<User | undefined> {
-    return this.userRepo._getUserByEmail(email)
+    return this.userRepo._getUserByEmail(email);
   }
-<<<<<<< HEAD
-  async getUserByUsername(userName: string): Promise<User | undefined> {
-    return await this.db.get('SELECT * FROM users WHERE userName=?', userName);
-  }
-
-  // Filme======
-  async allFilmesList250(): Promise<Filme[]> {
-    return await this.db.all('SELECT * FROM filmes  ');
-  }
-  async filmesList250(): Promise<Filme[]> {
-    return await this.db.all(
-      'SELECT * FROM filmes WHERE list250 > 0 AND list250 <= 250 ORDER BY list250 ASC'
-    );
-||||||| 11d24a7
-  async getUserByUsername(userName: string): Promise<User | undefined> {
-    return await this.db.get('SELECT * FROM users WHERE userName=?', userName);
-  }
-  // Filme======
-  async allFilmesList250(): Promise<Filme[]> {
-    return await this.db.all('SELECT * FROM filmes  ');
-  }
-  async filmesList250(): Promise<Filme[]> {
-    return await this.db.all(
-      'SELECT * FROM filmes WHERE list250 > 0 AND list250 <= 250 ORDER BY list250 ASC'
-    );
-=======
   getUserByUsername(userName: string): Promise<User | undefined> {
-    return this.userRepo._getUserByUsername(userName)
->>>>>>> test
+    return this.userRepo._getUserByUsername(userName);
   }
 
   // filmes========
 
   allFilmesList250(): Promise<Filme[]> {
-    return this.filmeRepo._allFilmesList250()
+    return this.filmeRepo._allFilmesList250();
   }
   filmesList250(): Promise<Filme[]> {
-    return this.filmeRepo._filmesList250()
+    return this.filmeRepo._filmesList250();
   }
   createFilme(filme: Filme): Promise<void> {
-    return this.filmeRepo._cerateFilme(filme)
+    return this.filmeRepo._cerateFilme(filme);
   }
 
   getFilmeById(id: string): Promise<Filme | undefined> {
-    return this.filmeRepo._getFilmeById(id)
+    return this.filmeRepo._getFilmeById(id);
   }
   getFilmeByOrignalName(originalName: string): Promise<Filme | undefined> {
-    return this.filmeRepo._getFilmeByOrignalName(originalName)
+    return this.filmeRepo._getFilmeByOrignalName(originalName);
   }
   updateFilmeList250(
     originalName: string,
-    list250: number | undefined,
+    list250: number | undefined
   ): Promise<void> {
-    return this.filmeRepo._updateFilmeList250(originalName, list250)
+    return this.filmeRepo._updateFilmeList250(originalName, list250);
   }
   updateFilme(filme: Filme): Promise<void> {
-    return this.filmeRepo._updateFilme(filme)
+    return this.filmeRepo._updateFilme(filme);
   }
   deleteFilme(id: string): Promise<void> {
-    return this.filmeRepo._deleteFilme(id)
+    return this.filmeRepo._deleteFilme(id);
   }
 
   //watchelib===========
   addToWatchedList(userId: string, filmeId: string): Promise<void> {
-    return this.watchelibRepo._addToWatchedList(userId, filmeId)
+    return this.watchelibRepo._addToWatchedList(userId, filmeId);
   }
   removeFromWatchedList(userId: string, filmeId: string): Promise<void> {
-    return this.watchelibRepo._removeFromWatchedList(userId, filmeId)
+    return this.watchelibRepo._removeFromWatchedList(userId, filmeId);
   }
   getWatchedUserList(userId: string): Promise<Mywatched[]> {
-    return this.watchelibRepo._getWatchedUserList(userId)
+    return this.watchelibRepo._getWatchedUserList(userId);
   }
   getWatchedUserFilme(userId: string, filmeId: string) {
-    return this.watchelibRepo._getWatchedUserFilme(userId, filmeId)
+    return this.watchelibRepo._getWatchedUserFilme(userId, filmeId);
   }
   addToLibrary(userId: string, filmeId: string): Promise<void> {
-    return this.watchelibRepo._addToLibrary(userId, filmeId)
+    return this.watchelibRepo._addToLibrary(userId, filmeId);
   }
   removeFromLibrary(userId: string, filmeId: string): Promise<void> {
-    return this.watchelibRepo._removeFromLibrary(userId, filmeId)
+    return this.watchelibRepo._removeFromLibrary(userId, filmeId);
   }
   getLibraryUserList(userId: string): Promise<Mylibrary[]> {
-    return this.watchelibRepo._getLibraryUserList(userId)
+    return this.watchelibRepo._getLibraryUserList(userId);
   }
   getLibraryUserFilme(
     userId: string,
-    filmeId: string,
+    filmeId: string
   ): Promise<Mylibrary | undefined> {
-    return this.watchelibRepo._getLibraryUserFilme(userId, filmeId)
+    return this.watchelibRepo._getLibraryUserFilme(userId, filmeId);
   }
 }
